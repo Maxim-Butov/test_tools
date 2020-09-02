87f45ba1c7c3694175cd32002d35936d1d888153
640K_ought_to_be_enough <67098694+Maxim-Butov@users.noreply.github.com>
2020-09-02 14:33:35 +0300

    Update testext.js

--- testext.js
+++ testext.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name         script doing nothing
-// @version      0.5
+// @version      0.6
 // @description  what a shame
 // @match        *://*/*e
 // @grant        unsafeWindow
 // @run-at       document-start
 // ==/UserScript==
 
+(function() {
+
+})();
 (function() {
 
 })();
